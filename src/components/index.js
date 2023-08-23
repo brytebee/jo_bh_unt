c2e944e0be109af3b5732a932e50c532ae91015a
Bright Atsighi <brytebee@gmail.com>
2023-08-23 18:44:39 +0100

    Fixed lints

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,10 @@
 /* eslint-disable import/no-cycle */
 import ComingSoon from './Comming Soon/ComingSoon';
-
 import BrandButton from './button/BrandButton';
 import Input from './input/Input';
 import MiniNav from './Comming Soon/MiniNav';
 import ErrorPage from './NotFound/ErrorPage';
 
 export {
-  ComingSoon, MiniNav, BrandButton, Input, ErrorPage
+  ComingSoon, MiniNav, BrandButton, Input, ErrorPage,
 };
